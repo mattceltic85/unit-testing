be31c4f0d09b7b3c408b3b1af1497b1a0354ad5b
Matt <matthewgarrity85@mac.com>
2018-06-11 17:42:30 +0100

    6 tests completed with Ethan

--- src/index.js
+++ src/index.js
@@ -3,3 +3,43 @@ function add(a, b){
 }
 
 exports.add = add;
+
+function longest(arr){
+  const result = arr.sort(function(a, b){
+    return b.length - a.length;
+  })
+  return result[0];
+}
+
+exports.longest = longest;
+
+function l337(string){
+  let newString = string
+    .replace('l','1')
+    .replace('z','2')
+    .replace('e','3')
+    .replace('a','4')
+    .replace('s','5')
+    .replace('g','6')
+    .replace('t','7')
+    .replace('b','8')
+    .replace('q','9')
+    .replace('o','0');
+  return newString;
+}
+
+exports.l337 = l337;
+
+function uniqueStrings(arr){
+  return Array.from(new Set(arr));
+}
+
+exports.uniqueStrings = uniqueStrings;
+
+function DeveloperObject(name, languages) {
+  this.name = name;
+  this.languages = languages;
+
+}
+
+exports.DeveloperObject = DeveloperObject;
--- test/index.test.js
+++ test/index.test.js
@@ -5,3 +5,29 @@ test('Addition', function(){
   const result = functions.add(2, 3);
   expect(result).toBe(expected);
 });
+
+test('LongestString',function(){
+  const expected = 'Banana';
+  const result = functions.longest(['Apple','Banana','Orange']);
+  expect(result).toBe(expected);
+})
+
+test('testl337',function(){
+  const expected = '1234567890'
+  const result = functions.l337('lzeasgtbqo');
+  expect(result).toBe(expected);
+})
+
+test('uniqueStringsTest',function(){
+  const expected = (['a', 'b', 'c', 'd', 'e', 'f', 'g']);
+  const result = functions.uniqueStrings(['a', 'b', 'a', 'b', 'c', 'd', 'e', 'f', 'g']);
+  expect(result).toEqual(expected);
+})
+
+test('DeveloperTest', function(){
+  const expected = "Python"
+  const result = new functions.DeveloperObject("Ethan",["Java","Ruby","Python"]);
+
+  expect(result.languages[2]).toBe("Python")
+  expect(result.name).toBe("Ethan")
+})
